3d524e3ca4edab3962d59226e39cbe4173263856
russellgoldman <russellgoldman1@gmail.com>
2019-10-16 03:08:17 -0400

    Webscraper build fix

--- webscraper/build/src/academic_calendar/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var main = function (coursePermission, programPermission) {
-    console.log("Course Scraping: " + coursePermission);
-    console.log("Program Scraping: " + programPermission);
-};
-exports["default"] = main;
--- webscraper/build/src/course_offerings/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var main = function (courseOfferingPermission) {
-    console.log("Course Offering Scraping: " + courseOfferingPermission);
-};
-exports["default"] = main;
