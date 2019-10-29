baa0f11d70dbbd5974953813d3df44b193657ed3
Ralph Suyat <Suyat.Ralph@gmail.com>
2019-10-28 21:07:13 -0400

    XML and Java files for registration page 1
    Signed-off-by: Ralph Suyat <Suyat.Ralph@gmail.com>

---
+++ android/app/src/main/java/com/example/cp470_group_project/Regs_comp_pg1.java
@@ -0,0 +1,15 @@
+package com.example.cp470_group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Regs_comp_pg1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regs_comp_pg1);
+
+    }
+}
