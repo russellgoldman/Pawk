26dfcb8049757bc8e163e7600794129df7d234d1
Brian <hane1750@mylaurier.ca>
2019-11-01 09:29:17 -0400

    Add files via upload
    To be updated, work in progress

---
+++ potus_parse_prog.js
@@ -0,0 +1,16 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+
+const potusParse = function(url) {
+  return rp(url)
+    .then(function(html) {
+      return {
+        program: $('.firstHeading', html).text(),
+      };
+    })
+    .catch(function(err) {
+      //handle error
+    });
+};
+
+module.exports = potusParse;
