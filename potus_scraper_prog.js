45412c6f15424267575dd3668223599098cd0d12
Brian <hane1750@mylaurier.ca>
2019-11-05 00:57:18 -0500

    updated to request promise
    file writing doesnt work yet

--- potus_scraper_prog.js
+++ potus_scraper_prog.js
@@ -1,22 +1,31 @@
-const puppeteer = require('puppeteer');
+const rp = require('request-promise');
 const $ = require('cheerio');
+const potusParse = require('./potusParse');
 const url = 'https://academic-calendar.wlu.ca/section.php?cal=1&s=939&y=79';
+var fs = require("fs");
+var file;
+file = fs.createWriteStream("data.txt");
 
-puppeteer
-  .launch()
-  .then(function(browser) {
-    return browser.newPage();
-  })
-  .then(function(page) {
-    return page.goto(url).then(function() {
-      return page.content();
-    });
-  })
+rp(url)
   .then(function(html) {
-    $('td > a', html).each(function() {
-      console.log($(this).text());
-    });
+    //success!
+    const urls = [];
+    for (let i = 0; i < 78; i++) {
+      urls.push($('td > a', html)[i].attribs.href);
+    }
+    return Promise.all(
+      urls.map(function(url) {
+        return potusParse('https://academic-calendar.wlu.ca/section.php?cal=1&s=939&y=79' + url);
+      })
+    );
+  })
+  .then(function(presidents) {
+    //console.log(presidents);
+	file.on('error', function(err) { Console.log(err) });
+	presidents.forEach(value => file.write(`${value}\r\n`));
+	file.end();
   })
   .catch(function(err) {
     //handle error
+    console.log(err);
   });
