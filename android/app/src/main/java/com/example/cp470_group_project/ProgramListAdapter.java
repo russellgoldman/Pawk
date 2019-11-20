162c5356ecda5bf954783259588336a94416de1b
Michelle Wong <wong2580@mylaurier.ca>
2019-11-19 23:03:28 -0500

    Implemented listView for explore program page

---
+++ android/app/src/main/java/com/example/cp470_group_project/ProgramListAdapter.java
@@ -0,0 +1,57 @@
+package com.example.cp470_group_project;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Button;
+
+/*
+    This is for the exploreProgram area
+ */
+public class ProgramListAdapter extends ArrayAdapter {
+    // references the activity (stores activity the list view is on)
+    private final Activity context;
+
+    // stores program names
+    private final String[] nameArray;
+
+    //stores program description
+    private final String[] infoArray;
+
+    /*
+           Constructor
+
+     */
+    public ProgramListAdapter(Activity context, String[] nameArrayParam, String[] infoArrayParam){
+        super(context,R.layout.listview_row,nameArrayParam);
+
+        this.context = context;
+        this.infoArray = infoArrayParam;
+        this.nameArray = nameArrayParam;
+    }
+
+    /*
+        Used by app to populate data into each row
+     */
+    public View getView(int position, View view, ViewGroup parent){
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.listview_row, null,true);
+
+        // Get references to objects in listview_row
+        TextView nameTextField = rowView.findViewById(R.id.programTitleTextView);
+        TextView infoTextField = rowView.findViewById(R.id.programDescTextView);
+        Button learnButton = rowView.findViewById(R.id.learnMoreButton);
+
+        // sets values of the objects to values from array
+        nameTextField.setText(nameArray[position]);
+        infoTextField.setText(infoArray[position]);
+
+        return rowView;
+    };
+
+
+}
---
+++ android/app/src/main/java/com/example/cp470_group_project/programPage.java
@@ -0,0 +1,27 @@
+package com.example.cp470_group_project;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Button;
+
+
+public class programPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_program_page);
+
+        String savedExtra = getIntent().getStringExtra("programName");
+        TextView myText = (TextView) findViewById(R.id.programTitleTextView);
+        myText.setText(savedExtra);
+    }
+}
