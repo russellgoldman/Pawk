1c1e8b1f43c2567000340b2ad4e701fe042ca4f1
Pirajeev <pirajeev13@hotmail.com>
2019-11-25 23:46:53 -0500

    Finalized Course Scraper

--- webscraper/src/courses/iterator.js
+++ webscraper/src/courses/iterator.js
@@ -3,18 +3,20 @@ const $ = require('cheerio');
 const courseParse = require('./justCourses.js');
 const url = 'https://academic-calendar.wlu.ca/section.php?cal=1&s=939&y=79';
 var links;
-const help = []
-
+const help = [];
 rp(url)
 .then(function(html) {
     //success!
     var wikiUrls = [];
     var length = $('tbody td > a',"#postermain", html).length;
-    //console.log(length);
     for (let i = 0; i < length; i++) {
         links = $('tbody td > a',"#postermain", html)[i].attribs.href;
-        if (links.charAt(0)=="/" && links.substring(0,2)!="/g") {
-            wikiUrls.push($('tbody td > a',"#postermain", html)[i].attribs.href);
+     
+        if (links.charAt(0)=="h" && links.substring(0,2)!="/g") {
+            course = $('tbody td > a',"#postermain", html)[i].attribs.href;
+            if (!course.startsWith("http://info.wlu.ca")) {
+                wikiUrls.push(course);
+            }
         }
     }
 
@@ -24,7 +26,7 @@ rp(url)
         console.log(courses.length);
 
         courses.forEach((el, index) => {
-            //console.log(el);
+            
             courseParse(el);
         })
     }
@@ -36,36 +38,36 @@ function getCourseLinks(items, callback) {
     var itemsLength = items.length;
     var select;
     var courses = []
-    var requests = 0;
+    var requests = 0;    
 
     for (var i = 0; i < itemsLength; i++) {
-        newurl = "https://academic-calendar.wlu.ca" + items[i];
-    
+        newurl = items[i];
+     
         rp(newurl)
         .then(function(html) {
             requests++
-            
+
             select = $('caption', html).text();
+          
             if (select =="Course Offerings") {
                 len = $('td>a',".zebra",html).length;
                 for(let i = 0; i < len; i++){
                    // select1 = $('td>a',".zebra",html)[i].attribs.href;
                     select2 =$('td>a',html)[i].attribs.href;
+                    //console.log(select2);
                     if (!courses.includes("https://academic-calendar.wlu.ca/" + select2) && select2.charAt(0)=="c"){
                         //console.log("https://academic-calendar.wlu.ca/" + select2)
                         courses.push("https://academic-calendar.wlu.ca/" + select2);
                     }
                 }         
             }
-            
             // once all the requests are done, initiate the callback function
-            if (requests === itemsLength-1) {
-                console.log(requests);
-                console.log(courses.length)
+            if (requests === 122) {
+  
                 callback(courses)
             }
         }).catch(function(err) {
-
+            console.log(err)
         });
     }
 }
--- webscraper/src/courses/justCourses.js
+++ webscraper/src/courses/justCourses.js
@@ -1,8 +1,9 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
+const mutator = require('./mutator');
 
 //const url = 'https://academic-calendar.wlu.ca/course.php?c=51919&cal=1&d=2022&s=931&y=79';
-//const url = 'https://academic-calendar.wlu.ca/course.php?c=53062&cal=1&d=2070&s=932&y=79'
+const url = 'https://academic-calendar.wlu.ca/course.php?c=53062&cal=1&d=2070&s=932&y=79'
 //const url = 'https://academic-calendar.wlu.ca/course.php?c=53069&cal=1&d=2070&s=932&y=79';
 //const url = 'https://academic-calendar.wlu.ca/course.php?c=53500&cal=1&d=2107&s=934&y=79';
 //const url = "https://academic-calendar.wlu.ca/program.php?cal=1&d=2068&p=4475&s=932&y=79";
@@ -20,102 +21,124 @@ var lecturehours;
 var labhours;
 var description;
 var reqs;
+var prerequisiteDescription = null
+var prerequisiteCourses = null
+var corequisiteDescription = null
+var corequisiteCourses = null
+var exclusionsDescription = null
+var exclusionCourses = null
+var notes = null
 const courseParse = function(url){
-rp(url)
-  .then(function(html) {
-    code = $('h1',"#postercontent",html).html().split("<br>");
-    realcode = code[0];
-    console.log(realcode)
-    nametemp = code[1].split('<span style="font-weight:normal;">');
-    nametemp2 = nametemp[1].split("</span");
-    realname = nametemp2[0];
-  //  console.log(realname)
-    creditstemp = code[2].split('<span style="font-weight:normal; color:#000000;">');
-    creditstemp2 = creditstemp[1].split("</span");
-    realcredits = creditstemp2[0].substring(0,3);
-  //  console.log(realcredits);
-    initialhours = $("li",".hours",html).text();
-    if(initialhours.substring(0,3)==("Lec")){
-      lecturehours = initialhours.substring(20,21);
-      if(initialhours.substring(21,24)=="Lab"){
-        labhours = initialhours.substring(26,30);
-      }else{
-        labhours = null;
-      }
+  
+  try {
+    rp(url)
+      .then(function(html) {
+        code = $('h1',"#postercontent",html).html().split("<br>");
+        realcode = code[0];
+      // console.log(realcode)
+        nametemp = code[1].split('<span style="font-weight:normal;">');
+        nametemp2 = nametemp[1].split("</span");
+        realname = nametemp2[0];
+        //console.log(realname)
+        creditstemp = code[2].split('<span style="font-weight:normal; color:#000000;">');
+        creditstemp2 = creditstemp[1].split("</span");
+        realcredits = creditstemp2[0].substring(0,3);
+    // console.log(realcredits);
+        initialhours = $("li",".hours",html).text();
+        if(initialhours.substring(0,3)==("Lec")){
+          lecturehours = initialhours.substring(20,21);
+          if(initialhours.substring(21,24)=="Lab"){
+            labhours = initialhours.substring(26,30);
+          }else{
+            labhours = null;
+          }
 
-    }else if(initialhours.substring(0,3)==("Lab")){
-      lecturehours = null;
-      labhours = lecturehours.split(" ")[2]
-    }else{
-      lecturehours = null;
-      labhours = null;
-    }
-   // console.log(lecturehours);
-   // console.log(labhours);
-    description = $('p',"#postercontent",html).text();
-   // console.log(description);
-    reqs = $('dt',".reqs",html).text().split(/(?=[A-Z])/);
-    //console.log(reqs);
-    length = $('dt', '.reqs', html).children().length
-    //console.log(length);
-    header = $('dt', '.reqs', html).first()
+        }else if(initialhours.substring(0,3)==("Lab")){
+          lecturehours = null;
+          labhours = lecturehours.split(" ")[2]
+        }else{
+          lecturehours = null;
+          labhours = null;
+        }
+      // console.log(lecturehours);
+      // console.log(labhours);
+        description = $('p',"#postercontent",html).text();
+      // console.log(description);
+        reqs = $('dt',".reqs",html).text().split(/(?=[A-Z])/);
+        //console.log(reqs);
+        length = $('dt', '.reqs', html).children().length
+        //console.log(length);
+        header = $('dt', '.reqs', html).first()
 
-    prerequisiteDescription = null
-    prerequisiteCourses = null
-    corequisiteDescription = null
-    corequisiteCourses = null
-    exclusionsDescription = null
-    exclusionCourses = null
-    notes = null
+        for (i = 0; i < length * 2; i++) {
+          headerCourses = header.next()
 
-    for (i = 0; i < length * 2; i++) {
-      headerCourses = header.next()
+          switch (header.text()) {
+            case "Prerequisites":
+              prerequisiteDescription = headerCourses.text()
+              prerequisiteCourses = []
+              headerCourses.children('a').each(function(i, elem) {
+                prerequisiteCourses.push($(this).text());
+              })
+              
+              //console.log(prerequisiteDescription)
+            //  console.log(prerequisiteCourses)
+              break
+            case "Co-requisites":
+              corequisiteDescription = headerCourses.text()
+              corequisiteCourses = []
+              headerCourses.children('a').each(function(i, elem) {
+                corequisiteCourses.push($(this).text());
+              })
+              
+            // console.log(corequisiteDescription)
+            // console.log(corequisiteCourses)
+              break
+            case "Exclusions":
+              exclusionsDescription = headerCourses.text()
+              exclusionCourses = []
+              headerCourses.children('a').each(function(i, elem) {
+                exclusionCourses.push($(this).text());
+              })
 
-      switch (header.text()) {
-        case "Prerequisites":
-          prerequisiteDescription = headerCourses.text()
-          prerequisiteCourses = []
-          headerCourses.children('a').each(function(i, elem) {
-            prerequisiteCourses.push($(this).text());
-          })
+              //console.log(exclusionsDescription)
+              //console.log(exclusionCourses)
+              break
+            case "Notes":
+              notes = headerCourses.text()
+
+            //  console.log(notes)
+              break
+          }
           
-          //console.log(prerequisiteDescription)
-        //  console.log(prerequisiteCourses)
-          break
-        case "Co-requisites":
-          corequisiteDescription = headerCourses.text()
-          corequisiteCourses = []
-          headerCourses.children('a').each(function(i, elem) {
-            corequisiteCourses.push($(this).text());
-          })
-          
-         // console.log(corequisiteDescription)
-         // console.log(corequisiteCourses)
-          break
-        case "Exclusions":
-          exclusionsDescription = headerCourses.text()
-          exclusionCourses = []
-          headerCourses.children('a').each(function(i, elem) {
-            exclusionCourses.push($(this).text());
-          })
+          header = headerCourses.next()
+        }
+        description += notes;
 
-          //console.log(exclusionsDescription)
-          //console.log(exclusionCourses)
-          break
-        case "Notes":
-          notes = headerCourses.text()
+        course = {
+          code: realcode,
+          name: realname,
+          credits: parseFloat(realcredits),
+          lecture_hours: parseFloat(lecturehours),
+          lab_hours:parseFloat(labhours),
+          description,
+          prereq_desc: prerequisiteDescription,
+          prereq_courses: prerequisiteCourses,
+          coreq_desc: corequisiteDescription,
+          coreq_courses: corequisiteCourses,
+          exc_desc: exclusionsDescription,
+          exc_courses: exclusionCourses
+        };
 
-        //  console.log(notes)
-          break
-      }
-      
-      header = headerCourses.next()
-    }
-    description += notes;
-  //  console.log(description);
-  })
-  .catch(function(err) {
-    //handle error
-  });
+        mutator.createCourse(course)
+      //  console.log(description);
+      })
+      .catch(function(err) {
+        //handle error
+      });
+  } catch (err) {
+    //console.log(err)
+  }
 }
-  module.exports = courseParse;
+
+module.exports = courseParse;
