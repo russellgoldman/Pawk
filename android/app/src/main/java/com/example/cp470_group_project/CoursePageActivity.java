e0efb69ff9c2ff281d586b6ec9ba6d05b3cb9a55
russellgoldman <russellgoldman1@gmail.com>
2019-11-28 03:14:54 -0500

    Course Page scrolling works

--- android/app/src/main/java/com/example/cp470_group_project/CoursePageActivity.java
+++ android/app/src/main/java/com/example/cp470_group_project/CoursePageActivity.java
@@ -5,19 +5,72 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class CoursePageActivity extends AppCompatActivity {
     final String ACTIVITY_NAME = "CoursePageActivity";
 
-    final boolean prerequisites = true;
-    final boolean corequisites = false;
-    final boolean exclusions = true;
+    public class Course implements Parcelable {
+        public String code;
+        public int rating;
+
+        public Course(String code, int rating) {
+            this.code = code;
+            this.rating = rating;
+        }
+
+        public Course(Parcel source) {
+            code = source.readString();
+            rating = source.readInt();
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeString(code);
+            dest.writeInt(rating);
+        }
+
+        public final Creator<Course> CREATOR = new Creator<Course>() {
+            @Override
+            public Course[] newArray(int size) {
+                return new Course[size];
+            }
+
+            @Override
+            public Course createFromParcel(Parcel source) {
+                return new Course(source);
+            }
+        };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_page);
+
+        // prerequisites
+        ArrayList<Course> coursePrerequisites = new ArrayList<Course>();
+        coursePrerequisites.add(new Course("CP102", 4));
+        coursePrerequisites.add(new Course("CP104", 5));
+        coursePrerequisites.add(new Course("MA103", 3));
+
+        // corequisites
+        ArrayList<Course> courseCorequisites = new ArrayList<Course>();
+        courseCorequisites.add(new Course("CP101", 2));
+        courseCorequisites.add(new Course("CP102", 4));
+
+        // exclusions
+        ArrayList<Course> courseExclusions = new ArrayList<Course>();
+        courseExclusions.add(new Course("CP202", 3));
 
         // add CourseAcquiredRatingFragment
         Fragment courseRatings = new CourseAcquiredRatingFragment();
@@ -29,17 +82,18 @@ public class CoursePageActivity extends AppCompatActivity {
         // add CourseRequisitesFragment
         transaction = getSupportFragmentManager().beginTransaction();
 
-        if (prerequisites) {
+        if (coursePrerequisites.size() > 0) {
             Fragment prerequisites = new CourseRequisitesFragment();
             Bundle data = new Bundle();
 
             data.putString("id", "prerequisites");
             data.putString("title", "Prerequisites");
+            data.putParcelableArrayList("prerequisites", coursePrerequisites);
             prerequisites.setArguments(data);
 
             transaction.replace(R.id.course_prerequisites, prerequisites);
         }
-        if (corequisites) {
+        if (courseCorequisites.size() > 0) {
             Log.i(ACTIVITY_NAME, "coreqs");
 
             Fragment corequisites = new CourseRequisitesFragment();
@@ -47,11 +101,12 @@ public class CoursePageActivity extends AppCompatActivity {
 
             data.putString("id", "corequisites");
             data.putString("title", "Corequisites");
+            data.putParcelableArrayList("corequisites", courseCorequisites);
             corequisites.setArguments(data);
 
             transaction.replace(R.id.course_corequisites, corequisites);
         }
-        if (exclusions) {
+        if (courseExclusions.size() > 0) {
             Log.i(ACTIVITY_NAME, "exclusions");
 
             Fragment exclusions = new CourseRequisitesFragment();
@@ -59,6 +114,7 @@ public class CoursePageActivity extends AppCompatActivity {
 
             data.putString("id", "exclusions");
             data.putString("title", "Exclusions");
+            data.putParcelableArrayList("exclusions", courseExclusions);
             exclusions.setArguments(data);
 
             transaction.replace(R.id.course_exclusions, exclusions);
--- android/app/src/main/java/com/example/cp470_group_project/CourseRequisitesFragment.java
+++ android/app/src/main/java/com/example/cp470_group_project/CourseRequisitesFragment.java
@@ -1,16 +1,22 @@
 package com.example.cp470_group_project;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -19,11 +25,6 @@ import android.widget.TextView;
 public class CourseRequisitesFragment extends Fragment {
     public String id;
     public String title;
-
-    public CourseRequisitesFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,7 +41,45 @@ public class CourseRequisitesFragment extends Fragment {
         id = extras.getString("id");
         title = extras.getString("title");
 
+        // get requisites array
+        ArrayList<CoursePageActivity.Course> courseRequisites = extras.getParcelableArrayList(id);
+
         TextView titleView = getView().findViewById(R.id.requisite_title);
         titleView.setText(title);
+
+        TextView requisiteNumView = getView().findViewById(R.id.requisite_num);
+        requisiteNumView.setText(Integer.toString(courseRequisites.size()));
+
+        // create ListView adapter
+        CourseRequisiteAdapter adapter = new CourseRequisiteAdapter(getActivity(), courseRequisites);
+        ListView requisiteListView = getView().findViewById(R.id.requisite_courses);
+        requisiteListView.setAdapter(adapter);
+    }
+
+    /*
+        Reference
+        https://guides.codepath.com/android/Using-an-ArrayAdapter-with-ListView
+    */
+    public class CourseRequisiteAdapter extends ArrayAdapter<CoursePageActivity.Course> {
+        public CourseRequisiteAdapter(Context context, ArrayList<CoursePageActivity.Course> courses) {
+            super(context, 0, courses);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            CoursePageActivity.Course course = getItem(position);
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.course_requisite_item, parent, false);
+            }
+
+            TextView code = convertView.findViewById(R.id.code);
+            TextView rating = convertView.findViewById(R.id.rating);
+
+            code.setText(course.code);
+            //rating.setText(course.rating);
+
+            return convertView;
+        }
     }
 }
