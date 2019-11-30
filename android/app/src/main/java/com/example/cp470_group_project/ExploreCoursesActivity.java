e1fc7826892a55b37f1637db95593287345f25fe
russellgoldman <russellgoldman1@gmail.com>
2019-11-30 18:12:50 -0500

    Explore courses progress

---
+++ android/app/src/main/java/com/example/cp470_group_project/ExploreCoursesActivity.java
@@ -0,0 +1,14 @@
+package com.example.cp470_group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExploreCoursesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_courses);
+    }
+}
