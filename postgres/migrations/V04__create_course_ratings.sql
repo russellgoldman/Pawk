5502b5df69ac1d753cbab5e4a76354a53339f5e3
Michelle Wong <wong2580@mylaurier.ca>
2019-11-30 23:38:52 -0500

    Revert "Merge branch 'master' of https://github.com/russellgoldman/CP470-Group-Project"
    This reverts commit baa0c45422dac2e44a292496faf8075af033a940, reversing
    changes made to c485a1ce3f45e7fa07401eadb5ccf16cdc0e8f80.

--- postgres/migrations/V04__create_course_ratings.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE pawk.course_ratings (
-    course_code TEXT REFERENCES pawk.courses(code) ON DELETE RESTRICT,
-    rating_id INT UNIQUE PRIMARY KEY NOT NULL, 
-    rating INT NOT NULL
-);
-
-COMMENT ON TABLE pawk.course_ratings IS
-'Course ratings for Laurier Courses';
