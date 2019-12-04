2ef6a529ef39bf882425afcdaa5b87630b52ef6a
Ralph Suyat <Suyat.Ralph@gmail.com>
2019-12-04 11:57:21 -0500

    Completed frame for registration
    Signed-off-by: Ralph Suyat <Suyat.Ralph@gmail.com>

---
+++ android/app/src/main/java/com/example/cp470_group_project/registerPage3_1.java
@@ -0,0 +1,14 @@
+package com.example.cp470_group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registerPage3_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page3_1);
+    }
+}
