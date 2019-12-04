f7de73f46bb05e87b40f032e98d2b09fc0e5c7e0
Ralph Suyat <Suyat.Ralph@gmail.com>
2019-12-04 11:40:08 -0500

    Base frame for registration
    Signed-off-by: Ralph Suyat <Suyat.Ralph@gmail.com>

--- android/app/src/main/java/com/example/cp470_group_project/registerPage1.java
+++ android/app/src/main/java/com/example/cp470_group_project/registerPage1.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Regs_comp_pg1 extends AppCompatActivity {
+public class registerPage1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_regs_comp_pg1);
+        setContentView(R.layout.activity_register_page1);
 
     }
 }
