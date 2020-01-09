264f39a1e36b4cc587d9989f467098ae66e95a37
russellgoldman <russellgoldman1@gmail.com>
2020-01-09 17:20:25 -0500

    Refactored get areaStudyTable into separate asychronous function

--- webscraper/src/webscraper.ts
+++ webscraper/src/webscraper.ts
@@ -16,7 +16,48 @@ export default class Webscraper {
     private visitedStudies: Array<string> = [];
     private visitedDepartments: Array<string> = [];
 
-    courseScrape() {
+    async courseScrape() {
+        const areasOfStudyTable = await this.getAreasOfStudyTable()
+            .then(function(val) {
+                return val;
+            })
+            .catch((err) => {
+                return err;
+            });
+
+        if (areasOfStudyTable === null) {
+            throw new Error('Unable to fetch areas of study table');
+        }
+
+        areasOfStudyTable.find('a').each(function (i, e) {
+            const areaOfStudy = $(this);
+            if (areaOfStudy.attr('href').startsWith('https://academic-calendar.wlu.ca')) {
+                console.log(areaOfStudy.text());
+                console.log(areaOfStudy.attr('href'));
+            }
+        });
+
+        /* 
+        - Using this URL (href), fetch the resulting page and check if 'Course Offerings' is listed ...
+            If ( 'Course Offerings' is listed ), then the Area of Study is valid ...
+
+            -> Check if the Area of Study name is stored in visitedStudies
+                -> If it hasn't been visited / stored, scrape the courses listed AND append the area of study name
+                    found at the top of the page in red
+        
+            Else ...
+
+            -> See if 'Departments' is listed
+                -> If so, iterate through all Departments and observe whether they have been visited / stored in visitedDepartments
+                -> If a department hasn't been visited / stored, fetch the resulting page and check if 'Course Offerings' is listed
+                    -> If it is listed, complete the steps above for 'If 'Course Offerings' is listed'
+                    -> If not, go back to 'See if 'Departments' is listed'
+            -> If 'Departments' is NOT listed, iterate to the NEXT area of study on the root url
+        */
+        
+    }
+
+    async getAreasOfStudyTable(): Promise<any> {
         const options: Object = {
             uri: this.academicCalendarRoot,
             transform: function (body) {
@@ -24,49 +65,25 @@ export default class Webscraper {
             }
         }
 
-        // outer context
-        const outer = this;
-
-        rp(options)
+        return rp(options)
             .then(($) => {
+                let areasOfStudyTable;
+
                 $('h1').each(function (index, el1) {
                     if ($(this).text() == 'Areas of Study/Courses @ Laurier (Direct Links)') {
-                        const areasOfStudyTable = $(this).next().next();
-                        areasOfStudyTable.find('a').each(function (i, e) {
-                            const areaOfStudy = $(this);
-                            if (areaOfStudy.attr('href').startsWith('https://academic-calendar.wlu.ca')) {
-                                console.log(areaOfStudy.text());
-                                console.log(areaOfStudy.attr('href'));
-
-                            /* 
-                            - Using this URL (href), fetch the resulting page and check if 'Course Offerings' is listed ...
-                                If ( 'Course Offerings' is listed ), then the Area of Study is valid ...
-
-                                -> Check if the Area of Study name is stored in visitedStudies
-                                    -> If it hasn't been visited / stored, scrape the courses listed AND append the area of study name
-                                        found at the top of the page in red
-                            
-                                Else ...
-
-                                -> See if 'Departments' is listed
-                                    -> If so, iterate through all Departments and observe whether they have been visited / stored in visitedDepartments
-                                    -> If a department hasn't been visited / stored, fetch the resulting page and check if 'Course Offerings' is listed
-                                        -> If it is listed, complete the steps above for 'If 'Course Offerings' is listed'
-                                        -> If not, go back to 'See if 'Departments' is listed'
-                                -> If 'Departments' is NOT listed, iterate to the NEXT area of study on the root url
-                            */
-                            }
-                            
-                        });
-
-                        console.log(outer.visitedStudies);
+                        areasOfStudyTable = $(this).next().next();                        
                     }
                 });
+
+                if (areasOfStudyTable !== null || typeof areasOfStudyTable !== 'undefined') {
+                    return Promise.resolve(areasOfStudyTable);
+                } else {
+                    return Promise.reject(null);
+                }
             })
             .catch((err) => {
-                console.log(err);
-            });
-
+                return Promise.reject(err);
+            })
     }
 
     programScrape() {
